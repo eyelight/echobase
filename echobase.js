a58cb9b8466df167d4a82befb00b4750f8df6786
Jason Cochard <cochard@protonmail.com>
2021-04-25 03:20:35 -0700

    Added objects SoloMessage & WheelbaseMessage.

--- echobase.js
+++ echobase.js
@@ -6,6 +6,60 @@ soloMessage.wbMethod = "";
 soloMessage.wbAmount = "";
 soloMessage.wbDirection = "";
 
+class SoloMessage {
+    constructor(msgType, from, unit, topic) {
+        this.msgType = msgType;
+        this.from = from;
+        this.unit = unit;
+        this.topic = topic;
+    }
+/*    get msgType() {
+        return this.msgType;
+    }
+    get from() {
+        return this.from;
+    }
+    get unit() {
+        return this.unit;
+    }
+    get topic() {
+        return this.topic;
+    }*/
+    spit() {
+        spit("Type: " + this.msgType +
+        " / From: " + this.from + 
+        " / Unit: " + this.unit + 
+        " / Topic: " + this.topic + 
+        " / Method: " + this.wbMethod +
+        " / Amount: " + this.wbAmount +
+        " / Direction: " + this.wbDirection
+        );
+    }
+    send() {
+        publish(this.topic, JSON.stringify(this));
+    }
+}
+
+
+class WheelbaseMessage extends SoloMessage {
+    constructor(msgType, from, unit, topic, wbMethod, wbAmount, wbDirection, command) {
+        super(msgType, from, unit, topic);
+        this.wbMethod = wbMethod;
+        this.wbAmount = wbAmount;
+        this.wbDirection = wbDirection;
+        this.command = command;
+    }
+/*    get wbMethod() {
+        return this.wbMethod;
+    }
+    get wbAmount() {
+        return this.wbAmount;
+    }
+    get wbDirection() {
+        return this.wbDirection;
+    }*/
+}
+
 function showSoloMessage() {
     console.log(soloMessage);
 }
@@ -39,3 +93,18 @@ function overrideSoloMessageTopic(t) {
     spit("Overriding soloMessage.topic: " + t);
     soloMessage.topic = t;
 }
+
+function autoEnableButton(id, t) {
+    setTimeout(() => {
+        enableButton(id);
+    }, t);
+}
+
+function muteMessage() {
+    spit("You're muted — commands are not sent")
+}
+
+function avastMessage() {
+    spit("The robot is in a timeout");
+}
+
